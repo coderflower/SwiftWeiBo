583ed4eb979dc674406d9106a9ee6119573d7b98
Caiflower <longjuncai@meifabao.cn>
2016-12-12 22:51:05 +0800

    建立视图模型,封装加载微博数据方法

---
+++ 花菜微博/花菜微博/Classes/Model(模型)/CFStatus.swift
@@ -0,0 +1,17 @@
+//
+//  CFStatus.swift
+//  花菜微博
+//
+//  Created by 花菜ChrisCai on 2016/12/12.
+//  Copyright © 2016年 花菜ChrisCai. All rights reserved.
+//
+
+import UIKit
+import YYModel
+class CFStatus: NSObject {
+    var id: Int = 0
+    var text: String?
+    override var description: String {
+        return yy_modelDescription()
+    }
+}
---
+++ 花菜微博/花菜微博/Classes/ViewModel(视图模型)/CFStatusListViewModel.swift
@@ -0,0 +1,38 @@
+//
+//  CFStatusListViewModel.swift
+//  花菜微博
+//
+//  Created by 花菜ChrisCai on 2016/12/12.
+//  Copyright © 2016年 花菜ChrisCai. All rights reserved.
+//
+
+import Foundation
+
+
+/// 尾部数据列表属兔
+/*
+    父类的选择
+  - 如果类需要使用 'KVC' 或者字典转模型框架设置对象值,类就需要继承自NSObject
+  - 如果类只是包装一些代码逻辑(写了一些函数),可以不用任何父类,好处:更加清凉级
+ */
+/// 负责微博的数据处理
+class CFStatusListViewModel {
+    lazy var statusList = [CFStatus]()
+    
+    func loadStatus(completion: @escaping (_ isSuccess: Bool) -> ()) {
+        CFHTTPManager.shared.statusList { (list, isSuccess) in
+            // 字典转模型
+            guard let array = NSArray.yy_modelArray(with: CFStatus.self, json: list ?? []) as? [CFStatus] else {
+                completion(isSuccess)
+                return
+            }
+            
+            // 拼接数据
+            self.statusList += array
+            
+            // 完成回调
+            completion(isSuccess)
+            
+        }
+    }
+}
