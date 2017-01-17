54319b1ef869ab0566c5503be7120c493869f52c
花菜 <longjuncai@meifabao.cn>
2017-01-17 23:43:58 +0800

    完善数据缓存代码

--- 花菜微博/花菜微博/Classes/ViewModel(视图模型)/CFStatusListDAL.swift
+++ 花菜微博/花菜微博/Classes/ViewModel(视图模型)/CFStatusListDAL.swift
@@ -22,10 +22,35 @@ class CFStatusListDAL {
     ///   - max_id: 返回ID小于或等于max_id的微博
     ///   - completion: 完成回调
     class func loadStatus(since_id: Int64 = 0, max_id: Int64 = 0, completion: @escaping (_ list: [[String : AnyObject]]?, _ isSuccess: Bool) -> ()) {
-        // 检查本地数据，如果有直接返回
-        // 加载网络数据
-        // 将网络数据【字典数组】写入数据库
-        // 返回网络数据
+        // 0. 获取用户 id
+        guard let userId = CFNetworker.shared.userAccount.uid else {
+            return
+        }
+        // 1. 检查本地数据，如果有直接返回
+        let array = CFDBHelper.sharedHelper.loadStatus(userId: userId, since_id: since_id, max_id: max_id)
+        // 1.1判断数据
+        if array.count > 0 {
+            // 1.2 有数据直接返回
+            completion(array, true)
+            return
+        }
+        // 2. 加载网络数据
+        CFNetworker.shared.statusList { (json, isSuccess) in
+            if !isSuccess {
+                // 2.1网络加载失败返回 nil
+                completion(nil,false)
+                return
+            }
+            guard let json = json else {
+                completion(nil,isSuccess)
+                return
+            }
+            // 3. 将网络数据【字典数组】写入数据库
+            CFDBHelper.sharedHelper.updateStatus(userId: userId, array: json)
+            // 4. 返回网络数据
+            completion(json, isSuccess)
+            
+        }
         
     }
     
